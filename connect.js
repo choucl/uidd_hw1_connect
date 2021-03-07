a0bc2436b79e3295277a152ef3965ee894fc20e8
choucl <clc25600@gmail.com>
2021-03-07 22:10:13 +0800

    modified: add hand cursor change when hover over buttons add: scroll animation

---
+++ connect.js
@@ -0,0 +1,37 @@
+// refernce: https://gist.github.com/andjosh/6764939
+
+document.getElementById('sign-up-btn').onclick = function () {
+    scrollTo(document.documentElement, 150, 1250);   
+}
+
+document.getElementById('scroll-down-btn').onclick = function(){
+    scrollTo(document.documentElement, 1300, 1250);
+}
+    
+function scrollTo(element, to, duration) {
+    var start = element.scrollTop,
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
+        
+    var animateScroll = function(){        
+        currentTime += increment;
+        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if(currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        }
+    };
+    animateScroll();
+}
+
+//t = current time
+//b = start value
+//c = change in value
+//d = duration
+Math.easeInOutQuad = function (t, b, c, d) {
+t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+};
